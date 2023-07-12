55162dccd53926cd706b46df41607536482f9a05
Esther <est.karanja@gmail.com>
2023-07-12 16:08:37 -0700

    completed Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+//multiply is an equation whose multiplied results will equal 62
 
-// Write your code here
+let num1;
+let num2;
+num1 = new Number(num1);
+num2 = new Number(num2);
+let multiply = num1*num2; 
+multiply = 62;
+console.log(multiply);
+
+//random generates a random integer greater than 0
+let random;
+random = (Math.round((Math.random() *Number.MAX_SAFE_INTEGER) + 1));
+
+console.log(random);
+
+//mod is an equation that calculates a remainder that is equal to 4:
+let num3;
+let num4;
+num3 = new Number(num3);
+num4 = new Number(num4);
+let mod = num3%num4;
+mod = 4
+console.log(mod);
+
+//max will return 20 as the highest number in the set
+let max = Math.max();
+max = 20;
+
+console.log(max);
